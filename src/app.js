3dfd9785cf3cf31af3965e6b028dbb97bf8c2f57
me-x-mi <ezrqnkemboi@gmail.com>
2020-03-11 10:02:51 +0300

    chore: set up linter and pre-commits

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
-    res.status(200).send({
-        message: "welcome to Task Scheduling"
-    })
-})
+  res.status(200).send({
+    message: "Welcome to Task Scheduling"
+  });
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`listening to http://localhost/${process.env.PORT}`)
-})
+  console.log(`listening to http://localhost/${process.env.PORT}`);
+});
 
 module.exports = app;
--- test/app.js
+++ test/app.js
@@ -1,17 +1,14 @@
-import chai, { expect } from 'chai';
-import chaHttp from 'chai-http';
-import app from '../src/app';
+import chai, { expect } from "chai";
+import chaHttp from "chai-http";
+import app from "../src/app";
 
 chai.use(chaHttp);
 
 describe("Return App", () => {
-    it('should return success on / url', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                done();
-            });
-    })
-})
+  it("should return success on / url", async () => {
+    const result = await chai.request(app).get("/");
+    const { status, body } = result;
+    expect(status).to.be.equal(200);
+    expect(body.message).to.be.equal("Welcome to Task Scheduling");
+  });
+});

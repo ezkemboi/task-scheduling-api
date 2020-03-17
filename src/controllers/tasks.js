5420a817d67fecd53a1dd7df182c2092d7e4ea6b
me-x-mi <ezrqnkemboi@gmail.com>
2020-03-17 15:25:14 +0300

    set up pagination

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -2,10 +2,31 @@ import { TasksService } from "../services";
 
 export default class TasksController {
   static async getAssignedTasks(req, res) {
-    const tasks = await TasksService.getAssignedTasks();
+    // set default;
+    let pageNo;
+    let pageLimit;
 
-    if (!tasks || tasks.length < 1) {
-      res.status(404).send({
+    const { page, limit } = req.query;
+
+    if (!page || page === undefined && !limit || limit === undefined) {
+      pageNo = 0;
+      pageLimit = 5;
+    } else if (!page || page === undefined && limit) {
+      pageNo = 0;
+    } else if (!limit || limit === undefined && page) {
+      pageLimit = 5;
+    } else {
+      pageNo = page;
+      pageLimit = limit;
+    }
+
+    const data = await TasksService.getAssignedTasks(pageNo, pageLimit);
+
+    const { tasks, currentPage, perPage } = data;
+    const { rows, count } = tasks;
+
+    if (!rows || rows.length < 1) {
+      return res.status(404).send({
         error: {
           message: "Tasks not found"
         }
@@ -13,8 +34,10 @@ export default class TasksController {
     }
     // return all tasks
     return res.status(200).send({
-      totalTasks: tasks.length,
-      tasks
+      totalTasks: count,
+      page: currentPage,
+      perPage,
+      tasks: rows
     });
   }
 }
